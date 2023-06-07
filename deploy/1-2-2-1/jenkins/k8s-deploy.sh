5940a031a67dd827813d8305a5d94c97985a8f6f
k8s-1pro <k8s.1pro@gmail.com>
2023-06-07 16:23:30 +0900

    update

--- deploy/1-2-2-1/jenkins/k8s-deploy.sh
+++ deploy/1-2-2-1/jenkins/k8s-deploy.sh
@@ -1,4 +1,4 @@
-kubectl delete -f ./deploy/k8s/1-2-2-1/k8s/deployment.yaml
-kubectl apply -f ./deploy/k8s/1-2-2-1/k8s/deployment.yaml
+kubectl delete -f ./deploy/1-2-2-1/k8s/deployment.yaml
+kubectl apply -f ./deploy/1-2-2-1/k8s/deployment.yaml
 kubectl apply -f ./deploy/1-2-2-1/k8s/service.yaml
 kubectl apply -f ./deploy/1-2-2-1/k8s/hpa.yaml
