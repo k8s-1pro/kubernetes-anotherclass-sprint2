bda5ed92544cd7a277d3076cef790bcec13a754d
k8s-1pro <k8s.1pro@gmail.com>
2023-06-13 11:04:05 +0900

    update

--- build/1-2-2-1/jenkins/docker-build.sh
+++ build/1-2-2-1/jenkins/docker-build.sh
@@ -1,3 +1,3 @@
 cp /var/lib/jenkins/workspace/1-2-2-1-gradle-build/build/libs/app-0.0.1-SNAPSHOT.jar ./build/1-2-2-1/docker/app-0.0.1-SNAPSHOT.jar
-docker build -t 1pro/app-v1 ./build/1-2-2-1/docker
-docker push 1pro/app-v1
+docker build -t 1pro/app ./build/1-2-2-1/docker
+docker push 1pro/app
