5297c218a1aeab21e1c9010a76f9f3f8ca143d53
k8s-1pro <k8s.1pro@gmail.com>
2023-07-11 14:22:23 +0900

    update

---
+++ build/1-2-3-1/jenkins/docker-build.sh
@@ -0,0 +1,3 @@
+cp /var/lib/jenkins/workspace/1-2-3-1-gradle-build/build/libs/app-0.0.1-SNAPSHOT.jar ./build/1-2-3-1/docker/app-0.0.1-SNAPSHOT.jar
+docker build -t 1pro/api-tester:v1.0.0 ./build/1-2-3-1/docker
+docker push 1pro/api-tester:v1.0.0
